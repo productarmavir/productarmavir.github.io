286349152bbb2871edf9ed1ff0a5ac1cd47d8349
GerasHarutyunyan <76776768+GerasHarutyunyan@users.noreply.github.com>
2021-03-09 22:11:54 +0400

    ftgtv

---
+++ media6/script.js
@@ -0,0 +1,173 @@
+
+let gg=document.querySelectorAll(".anim77")
+let ff=document.querySelectorAll(".anim88")
+let rr=document.querySelectorAll(".anim99")
+let dd=document.querySelectorAll(".anim100")
+
+
+
+// for(let i=0;i<ftg.length;i++){
+// 	ftg[i].addEventListener("click",hov)
+// }
+
+
+
+
+function f(){
+	
+for(let i=0;i<=gg.length;i++){
+	gg[i].classList.add("juki"+(i+1))
+}
+}
+function t(){
+	for(let i=0;i<=gg.length;i++){
+	gg[i].classList.remove("juki"+(i+1))
+}
+}
+
+function q(){
+	for(let i=0;i<=ff.length;i++){
+	ff[i].classList.remove("juki"+(i+1))
+}
+}
+function p(){
+for(let i=0;i<=ff.length;i++){
+	ff[i].classList.add("juki"+(i+1))
+}
+}
+
+function ok(){
+	for(let i=0;i<=rr.length;i++){
+	rr[i].classList.remove("juki"+(i+1))
+}
+}
+function pok(){
+for(let i=0;i<=rr.length;i++){
+	rr[i].classList.add("juki"+(i+1))
+}
+}
+
+
+function ook(){
+	for(let i=0;i<=dd.length;i++){
+	dd[i].classList.remove("juki"+(i+1))
+}
+}
+function pook(){
+for(let i=0;i<=dd.length;i++){
+	dd[i].classList.add("juki"+(i+1))
+}
+}
+
+
+	$("#mek").click(function (){
+
+
+		$(".anim1").toggleClass("juki11");
+		$(".anim2").toggleClass("juki22");
+		$(".anim3").toggleClass("juki33");
+		$(".anim4").toggleClass("juki44");
+		$(".anim5").toggleClass("juki55");
+			$(".anim6").toggleClass("juki66");
+
+
+	$(".anim111").removeClass("juki11");
+		$(".anim222").removeClass("juki22");
+		$(".anim333").removeClass("juki33");
+		$(".anim444").removeClass("juki44");
+		$(".anim555").removeClass("juki55");
+		$(".anim666").removeClass("juki66");
+		$(".anim11").removeClass("juki11");
+		$(".anim22").removeClass("juki22");
+		$(".anim33").removeClass("juki33");
+		$(".anim44").removeClass("juki44");
+		$(".anim55").removeClass("juki55");
+
+		$(".anim1111").removeClass("juki11");
+		$(".anim2222").removeClass("juki22");
+		$(".anim3333").removeClass("juki33");
+		$(".anim4444").removeClass("juki44");
+		$(".anim5555").removeClass("juki55");
+	})
+
+	$("#erku").click(function (){
+		$(".anim11").toggleClass("juki11");
+		$(".anim22").toggleClass("juki22");
+		$(".anim33").toggleClass("juki33");
+		$(".anim44").toggleClass("juki44");
+		$(".anim55").toggleClass("juki55");
+
+
+			$(".anim111").removeClass("juki11");
+		$(".anim222").removeClass("juki22");
+		$(".anim333").removeClass("juki33");
+		$(".anim444").removeClass("juki44");
+		$(".anim555").removeClass("juki55");
+		$(".anim666").removeClass("juki66");
+
+		$(".anim1").removeClass("juki11");
+		$(".anim2").removeClass("juki22");
+		$(".anim3").removeClass("juki33");
+		$(".anim4").removeClass("juki44");
+		$(".anim5").removeClass("juki55");
+		$(".anim6").removeClass("juki66");
+		$(".anim1111").removeClass("juki11");
+		$(".anim2222").removeClass("juki22");
+		$(".anim3333").removeClass("juki33");
+		$(".anim4444").removeClass("juki44");
+		$(".anim5555").removeClass("juki55");
+	})
+		$("#ereq").click(function (){
+		$(".anim111").toggleClass("juki11");
+		$(".anim222").toggleClass("juki22");
+		$(".anim333").toggleClass("juki33");
+		$(".anim444").toggleClass("juki44");
+		$(".anim555").toggleClass("juki55");
+		$(".anim666").toggleClass("juki66");
+
+			$(".anim1").removeClass("juki11");
+		$(".anim2").removeClass("juki22");
+		$(".anim3").removeClass("juki33");
+		$(".anim4").removeClass("juki44");
+		$(".anim5").removeClass("juki55");
+			$(".anim6").removeClass("juki66");
+				$(".anim11").removeClass("juki11");
+		$(".anim22").removeClass("juki22");
+		$(".anim33").removeClass("juki33");
+		$(".anim44").removeClass("juki44");
+		$(".anim55").removeClass("juki55");
+			$(".anim1111").removeClass("juki11");
+		$(".anim2222").removeClass("juki22");
+		$(".anim3333").removeClass("juki33");
+		$(".anim4444").removeClass("juki44");
+		$(".anim5555").removeClass("juki55");
+	})
+
+
+	$("#chors").click(function (){
+		$(".anim1111").toggleClass("juki11");
+		$(".anim2222").toggleClass("juki22");
+		$(".anim3333").toggleClass("juki33");
+		$(".anim4444").toggleClass("juki44");
+		$(".anim5555").toggleClass("juki55");
+
+
+			$(".anim111").removeClass("juki11");
+		$(".anim222").removeClass("juki22");
+		$(".anim333").removeClass("juki33");
+		$(".anim444").removeClass("juki44");
+		$(".anim555").removeClass("juki55");
+		$(".anim666").removeClass("juki66");
+
+		$(".anim1").removeClass("juki11");
+		$(".anim2").removeClass("juki22");
+		$(".anim3").removeClass("juki33");
+		$(".anim4").removeClass("juki44");
+		$(".anim5").removeClass("juki55");
+			$(".anim6").removeClass("juki66");
+						$(".anim11").removeClass("juki11");
+		$(".anim22").removeClass("juki22");
+		$(".anim33").removeClass("juki33");
+		$(".anim44").removeClass("juki44");
+		$(".anim55").removeClass("juki55");
+	})
